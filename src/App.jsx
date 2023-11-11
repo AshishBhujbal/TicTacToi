5632607d6024f3575669381e52dc2334fd932ab5
Ashish Bhujbal <ashish.bhujbal500@gmail.com>
2023-11-11 20:04:34 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+import Game from "./componets/Game";
+function App()
+{
+  return(<Game />);
+}
+
+export default App;
---
+++ src/componets/Square.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export {Square};
